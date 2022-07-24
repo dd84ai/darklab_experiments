992439d4a99b7bd119fe22221c6760f76f46b6d4
dd84ai <dd84ai@gmail.com>
2022-07-24 18:17:38 +0200

    refactor

--- time_parser.py
+++ time_parser.py
@@ -9,13 +9,37 @@ def line_reader():
             yield line
 
 @dataclass(frozen=True)
-class ParsedDatetime:
-    month: str
+class Date:
+    month: int
     day: int
-    extra_number: int | None
+
+
+@dataclass(frozen=True)
+class Time:
     hours: int
     minutes: int
+    extra_number: int | None = None
 
+
+@dataclass(frozen=True)
+class ParsedDatetime:
+    date: Date
+    time: Time
+
+class ParsedDatetimeFactory:
+    def __new__(cls, *args, **kwargs: dict) -> ParsedDatetime:
+        is_source_dict = True if len(args) == 0 else False
+        return ParsedDatetime(
+            Date(
+                month=kwargs["month"] if is_source_dict else args[0],
+                day=kwargs["day"] if is_source_dict else args[1],
+            ),
+            Time(
+                extra_number=kwargs["extra_number"] if is_source_dict else args[2],
+                hours=kwargs["hours"] if is_source_dict else args[3],
+                minutes=kwargs["minutes"] if is_source_dict else args[4],
+            ),
+        )
 
 def parse_line(line) -> 'ParsedDatetime':
     regex_month = "([a-zA-Z]+)"
@@ -27,23 +51,13 @@ def parse_line(line) -> 'ParsedDatetime':
     regex = f"^\s*{regex_month}\s+{regex_day}\s+\({regex_extra_thing}{regex_hours}:{regex_minutes}\)$"
     result = re.search(regex, line)
     
-    return ParsedDatetime(
+    return ParsedDatetimeFactory(
         month=result.group(1),
         day=int(result.group(2)),
         extra_number=int(result.group(4)) if result.group(4) is not None else None,
         hours=int(result.group(5)),
-        minutes=int(result.group(6)),
+        minutes=int(result.group(6))
     )
-
-@dataclass(frozen=True)
-class Time:
-    hours: int
-    minutes: int
-
-@dataclass(frozen=True)
-class Date:
-    month: int
-    day: int
 
 
 class AggregatedTimeIntoDays:
@@ -51,29 +65,22 @@ class AggregatedTimeIntoDays:
         self._storage = {}
 
     def add(self, parsed_datetime: ParsedDatetime):
-        key = self._get_key(parsed_datetime)
-
+        key = parsed_datetime.date
         self._init_in_storage_if_not_exists(key)
 
         self._storage[key] = self._sum_new_and_previous_time(
             previous_time = self._storage[key],
-            extra_thing = parsed_datetime.extra_number,
-            new_hours = parsed_datetime.hours,
-            new_minutes = parsed_datetime.minutes
+            newtime = parsed_datetime.time,
         )
-
-    @staticmethod
-    def _get_key(parsed_datetime):
-        return Date(parsed_datetime.month, parsed_datetime.day)
     
     def _init_in_storage_if_not_exists(self, key):
         if key not in self._storage:
             self._storage[key] = Time(hours=0, minutes=0)
 
-    def _sum_new_and_previous_time(self, previous_time, extra_thing, new_hours, new_minutes):
-        bonus_time = 24 if extra_thing is not None else 0
-        summed_hours = previous_time.hours + new_hours + bonus_time
-        summed_minutes = previous_time.minutes + new_minutes
+    def _sum_new_and_previous_time(self, previous_time: Time, newtime: Time):
+        bonus_time = 24 if newtime.extra_number is not None else 0
+        summed_hours = previous_time.hours + newtime.hours + bonus_time
+        summed_minutes = previous_time.minutes + newtime.minutes
         return Time(
             hours = summed_hours + int(summed_minutes / 60),
             minutes= summed_minutes % 60,
@@ -105,22 +112,22 @@ class TestParser(unittest.TestCase):
     
     def test_parse_line(self):
         parsed = parse_line("Jul 2   (04:17)")
-        self.assertEqual(parsed, ParsedDatetime(month='Jul', day=2, extra_number=None, hours=4, minutes=17)) 
+        self.assertEqual(parsed, ParsedDatetimeFactory(month='Jul', day=2, extra_number=None, hours=4, minutes=17)) 
 
     def test_parse_tricky_line(self):
         parsed = parse_line("Jul 2   (1+12:44)")
-        self.assertEqual(parsed, ParsedDatetime(month='Jul', day=2, extra_number=1, hours=12, minutes=44)) 
+        self.assertEqual(parsed, ParsedDatetimeFactory(month='Jul', day=2, extra_number=1, hours=12, minutes=44)) 
 
     def test_aggregator(self):
         aggregated_time_per_day = AggregatedTimeIntoDays()
-        aggregated_time_per_day.add(ParsedDatetime("Jul", 2, None, 22, 50))
-        aggregated_time_per_day.add(ParsedDatetime("Jul", 2, None, 22, 50))
+        aggregated_time_per_day.add(ParsedDatetimeFactory("Jul", 2, None, 22, 50))
+        aggregated_time_per_day.add(ParsedDatetimeFactory("Jul", 2, None, 22, 50))
 
         self.assertEqual(next(iter(aggregated_time_per_day)), (Date(month='Jul', day=2), Time(hours=45, minutes=40)))
 
     def test_how_to_treat_extra_number(self):
         # like extra 24 hours?
         aggregated_time_per_day = AggregatedTimeIntoDays()
-        aggregated_time_per_day.add(ParsedDatetime("Jul", 2, 1, 23, 50))
+        aggregated_time_per_day.add(ParsedDatetimeFactory("Jul", 2, 1, 23, 50))
         self.assertEqual(next(iter(aggregated_time_per_day)), (Date(month='Jul', day=2), Time(hours=47, minutes=50)))
 
